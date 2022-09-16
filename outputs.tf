6a8bc9e0a04315acc688651a8daf20fdf6ee1d68
Young-ook Kim <youngook.kim@gmail.com>
2022-09-16 20:37:43 +0900

    chore(emr): formatting

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "id" {
-  value = "${aws_emr_cluster.emr.id}"
+  value = aws_emr_cluster.emr.id
 }
 
 output "svc_role_id" {
-  value = "${aws_iam_role.svc.id}"
+  value = aws_iam_role.svc.id
 }
 
 output "task_role_id" {
-  value = "${aws_iam_role.task.id}"
+  value = aws_iam_role.task.id
 }
