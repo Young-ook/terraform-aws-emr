ab9b773a3f7078da45959e750704378fb9e3963c
Young-ook Kim <youngook.kim@gmail.com>
2022-09-17 23:05:53 +0900

    chore(emr): reorganize files

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "id" {
-  value = aws_emr_cluster.emr.id
+  value = aws_emr_cluster.cp.id
 }
 
 output "svc_role_id" {
-  value = aws_iam_role.svc.id
+  value = aws_iam_role.cp.id
 }
 
 output "task_role_id" {
-  value = aws_iam_role.task.id
+  value = aws_iam_role.ng.id
 }
