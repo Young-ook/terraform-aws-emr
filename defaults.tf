c58dacd2bbdddfa02dfcf2ef303f1b5196d404b4
Young-ook Kim <youngook.kim@gmail.com>
2022-09-19 12:07:36 +0900

    fix(emr): upgrade default/minimum instance type

--- defaults.tf
+++ defaults.tf
@@ -2,11 +2,11 @@
 
 locals {
   default_master_node_groups = {
-    instance_type  = "m5.large"
+    instance_type  = "m5.xlarge"
     instance_count = 1
   }
   default_core_node_groups = {
-    instance_type  = "m5.large"
+    instance_type  = "m5.xlarge"
     instance_count = 3
   }
 }
