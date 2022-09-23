14854b84dd048df700be99d46e33f6dcbe5a6527
youngookkim <youngook.kim@gmail.com>
2022-09-23 17:54:49 +0900

    feat(emr): dynamic instance fleet config for core nodes (#6)
    * fix(emr): security group description
    
    * feat(emr): dynamic instance fleet config for core nodes

--- defaults.tf
+++ defaults.tf
@@ -6,7 +6,23 @@ locals {
     instance_count = 1
   }
   default_core_node_groups = {
-    instance_type  = "m5.xlarge"
-    instance_count = 3
+    instance_type                  = "m5.xlarge"
+    provisioned_on_demand_capacity = 1
+    provisioned_spot_capacity      = 1
+    target_on_demand_capacity      = 1
+    target_spot_capacity           = 1
   }
+  default_instance_type_config = {
+    bid_price_as_percentage_of_on_demand_price = 100
+    instance_type                              = "m5.xlarge"
+    weighted_capacity                          = 1
+    default_ebs_configs = {
+      size                 = 100
+      type                 = "gp2"
+      volumes_per_instance = 1
+    }
+  }
+  default_instance_type_configs = [
+    local.default_instance_type_config
+  ]
 }
