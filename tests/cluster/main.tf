38965f4d98e94649797f44e80962ca6d27e3eed2
Young-ook Kim <youngook.kim@gmail.com>
2023-05-07 12:27:52 +0900

    fix(emr): update the test case for emr cluster configs

--- tests/cluster/main.tf
+++ tests/cluster/main.tf
@@ -32,6 +32,36 @@ module "main" {
       }
     }
   }
+  master_node_groups = {}
+  core_node_groups = {
+    instance_type_configs = [
+      {
+        instance_type     = "m5.xlarge"
+        weighted_capacity = 2
+      },
+    ]
+  }
+  task_node_groups = {
+    target_on_demand_capacity = 1
+    target_spot_capacity      = 2
+    launch_specifications = {
+      spot_specification = {
+        allocation_strategy      = "capacity-optimized"
+        timeout_action           = "TERMINATE_CLUSTER"
+        timeout_duration_minutes = 15
+      }
+    }
+    instance_type_configs = [
+      {
+        instance_type     = "m5.xlarge"
+        weighted_capacity = 2
+      },
+      {
+        instance_type     = "m5.2xlarge"
+        weighted_capacity = 1
+      }
+    ]
+  }
 }
 
 resource "test_assertions" "pet_name" {
