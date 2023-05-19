9ceaed7ad835302198ba6c68ef6bb6116f4937fe
Young-ook Kim <youngook.kim@gmail.com>
2023-05-19 18:44:44 +0900

    feat(blueprint): pi pyspark job example

--- examples/blueprint/outputs.tf
+++ examples/blueprint/outputs.tf
@@ -1,12 +1,19 @@
-output "vpc" {
-  description = "Amazon VPC network"
-  value       = module.vpc
-}
-
 output "emr" {
   description = "Amazon EMR cluster"
   value = {
     emr-ec2 = module.emr-ec2
     emr-eks = module.emr-eks
+  }
+}
+
+output "apps" {
+  description = "Bash script to run a basic pyspark job on your virtual EMR cluster"
+  value = {
+    pyspark_pi = join(" ", [
+      "bash -e",
+      format("%s/apps/pi/basic-pyspark-job.sh", path.module),
+      format("-c %s", module.emr-eks.cluster["id"]),
+      format("-r %s", module.emr-eks.role["job"].arn),
+    ])
   }
 }
