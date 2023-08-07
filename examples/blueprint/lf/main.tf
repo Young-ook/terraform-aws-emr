f39ef1e8c6c8f9a44a7b063639ab0736af59be57
youngookkim <youngook.kim@gmail.com>
2023-08-07 14:43:33 +0900

    feat(blueprint) initial version of lake formation example (#40)
    * feat(blueprint) initial version of lake formation example
    
    * fix(blueprint): iam-role module upgrade

---
+++ examples/blueprint/lf/main.tf
@@ -0,0 +1,21 @@
+variable "s3_arn" {
+  type = string
+}
+
+### data lake
+module "datalake-admin" {
+  source           = "Young-ook/passport/aws//modules/iam-role"
+  version          = "0.0.9"
+  name             = "datalake-admin"
+  tags             = { desc = "data lake admin" }
+  session_duration = "7200"
+  policy_arns      = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
+
+resource "aws_lakeformation_data_lake_settings" "lf" {
+  admins = [module.datalake-admin.role.arn]
+}
+
+resource "aws_lakeformation_resource" "lf" {
+  arn = var.s3_arn
+}
