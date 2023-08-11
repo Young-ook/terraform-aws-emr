f07b77636b447241f09ca1b6631c603a3c715674
Young-ook Kim <youngook.kim@gmail.com>
2023-08-11 19:14:19 +0900

    feat(redshift): serverless test case

---
+++ modules/redshift/tests/serverless/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "Young-ook/vpc/aws"
+  version = "1.0.3"
+}
+
+module "main" {
+  source  = "../.."
+  vpc     = module.vpc.vpc.id
+  subnets = values(module.vpc.subnets["public"])
+  redshift_cluster = {
+    mode = "serverless"
+  }
+}
+
+resource "test_assertions" "pet_name" {
+  component = "pet_name"
+
+  check "pet_name" {
+    description = "default random pet name"
+    condition   = can(length(regexall("^redshift", module.main.cluster.name)) > 0)
+  }
+}
